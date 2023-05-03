05818925b6791f13015e160aae688c7d9a6c8a33
Claiyc <claiyc@outlook.com>
2023-05-03 18:24:49 +0200

    add authorization middleware

--- routes.ts
+++ routes.ts
@@ -1,10 +1,17 @@
 import express from 'express';
 import RedisService from './services/Redis.service';
+import { getAuthorizationMW } from './routes/middlewares';
 
 export default function(app: express.Application) {
     const router = express.Router();
     const redisService = RedisService.getInstance();
     redisService.init();
+
+    // middlewares
+    const authMW = getAuthorizationMW();
+
+    app.use(authMW);
+
     // call routes here
 
     // hello world
