9d359119c24c8ced7abad38f0e67d1b002aec52c
Claiyc <claiyc@outlook.com>
2023-05-03 17:33:11 +0200

    add RedisService

--- routes.ts
+++ routes.ts
@@ -1,12 +1,16 @@
 import express from 'express';
+import RedisService from './services/Redis.service';
 
 export default function(app: express.Application) {
     const router = express.Router();
+    const redisService = RedisService.getInstance();
+    redisService.init();
     // call routes here
 
     // hello world
-    router.get('/', (req, res) => {
-        res.send('Hello World!');
+    router.get('/', async (req, res) => {
+        await redisService.set('test', '.', { abc: 'def' });
+        res.send(await redisService.get('test', '.'));
     });
 
     app.use('/', router);
