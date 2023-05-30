966dba9d200e2405cb78a81485ef66b506d1927d
Claiyc <claiyc@outlook.com>
2023-05-30 09:58:48 +0200

    add cors

--- routes.ts
+++ routes.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import RedisService from './services/Redis.service';
+import cors from 'cors';
 import { getAuthorizationMW } from './routes/middlewares';
 
 // routes
@@ -9,6 +10,27 @@ export default function(app: express.Application) {
     const router = express.Router();
     const redisService = RedisService.getInstance();
     redisService.init();
+
+    app.use(cors({
+        origin: function(origin, callback) {
+            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+
+            // allow requests with no origin
+            if (!origin) {
+                return callback(null, true);
+            }
+            if (process.env.ALLOWED_ORIGINS) {
+                const allowedOrigins = process.env.ALLOWED_ORIGINS.split(',');
+                if (allowedOrigins.indexOf(origin) === -1) {
+                    return callback(new Error(msg), false);
+                } else {
+                    return callback(null, true);
+                }
+            }
+            return callback(new Error(), false);
+        },
+        credentials: true
+    }));
 
     // middlewares
     const authMW = getAuthorizationMW();
